d71dc00fb54475a98f3305cdcf4f33ecb772c10d
Akash Shyam <shyamiy@gmail.com>
2021-07-31 08:32:46 +0530

    ✨ feat(users): password encryption

--- server/src/models/Users.ts
+++ server/src/models/Users.ts
@@ -1,5 +1,6 @@
 import { Schema, model, Model } from 'mongoose';
 import { User } from '../types/users';
+import { hash } from 'bcryptjs';
 
 const schema = new Schema<User>(
 	{
@@ -9,6 +10,7 @@ const schema = new Schema<User>(
 		},
 		email: {
 			type: String,
+			unique: true,
 			required: true,
 		},
 		password: {
@@ -18,6 +20,10 @@ const schema = new Schema<User>(
 	},
 	{ toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
+
+schema.pre('save', async function (this: User) {
+	this.password = await hash(this.password, 12);
+});
 
 const User = model<User, Model<User>>('User', schema);
 
