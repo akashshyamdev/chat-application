ddd6670916c0779dc39dcd27fb265fc2f8a2abc8
Akash Shyam <shyamiy@gmail.com>
2021-08-01 16:25:05 +0530

    🐛  fix: moved components to containers(mistake)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-import Login from 'components/Auth/Login/Login';
+import Login from 'containers/Auth/Login/Login';
 import React from 'react';
 import { Route, Router } from 'react-router-dom';
-import Signup from './components/Auth/Signup/Signup';
 import history from './config/history';
+import Signup from './containers/Auth/Signup/Signup';
 
 function App() {
 	return (
