52397aff5e38ff61059d7ecfd5f753038ce50b4a
Akash Shyam <shyamiy@gmail.com>
2021-08-07 15:47:37 +0530

    ✨ feat(groups): get group

--- server/src/controllers/groups.ts
+++ server/src/controllers/groups.ts
@@ -1,6 +1,26 @@
 import { Request, Response } from 'express';
 import Group from '../models/Group';
 
+export async function getAllGroups(req: Request, res: Response) {
+	try {
+		const groups = await Group.find();
+
+		return res.status(200).json(groups);
+	} catch (error) {
+		return res.status(500).json(error);
+	}
+}
+
+export async function getGroup(req: Request, res: Response) {
+	try {
+		const group = await Group.findById(req.params.id);
+
+		return res.status(200).json(group);
+	} catch (error) {
+		return res.status(500).json(error);
+	}
+}
+
 export async function createGroup(req: Request, res: Response) {
 	try {
 		const group = await Group.create(req.body);
--- server/src/routes/group.ts
+++ server/src/routes/group.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
-import { createGroup } from '../controllers/groups';
+import { createGroup, getAllGroups, getGroup } from '../controllers/groups';
 
 const router = Router();
+
+router.get('/', getAllGroups);
+router.get('/:id', getGroup);
 
 router.post('/', createGroup);
 
