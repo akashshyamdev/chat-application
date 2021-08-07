86858570a1949eeefb32036b467df913c2828ad1
Akash Shyam <shyamiy@gmail.com>
2021-08-07 19:53:06 +0530

    ✨ feat(groups): create screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
-import Login from 'containers/Login/Login';
 import React from 'react';
 import { Route, Router } from 'react-router-dom';
 import history from './config/history';
+import GroupCreate from './containers/Groups/GroupCreate/GroupCreate';
+import GroupList from './containers/Groups/GroupList/GroupList';
+import Login from './containers/Login/Login';
 import Signup from './containers/Signup/Signup';
 
 function App() {
 	return (
 		<Router history={history}>
+			{/* Auth */}
 			<Route path='/signup' exact component={Signup} />
 			<Route path='/login' exact component={Login} />
+
+			{/* Groups */}
+			<Route path='/groups' exact component={GroupList} />
+			<Route path='/groups/create' exact component={GroupCreate} />
 		</Router>
 	);
 }
---
+++ client/src/containers/Groups/GroupCreate/GroupCreate.tsx
@@ -0,0 +1,48 @@
+import React, { ChangeEvent, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
+import Http from 'services/Http';
+
+interface FormData {
+	name: string;
+	email: string;
+	password: string;
+}
+
+export default function GroupCreate() {
+	const history = useHistory();
+
+	// Form
+	const [formData, setFormData] = React.useState<FormData>({ name: '', email: '', password: '' });
+
+	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const http = new Http('api/v1/groups');
+		http.create(formData);
+
+		setTimeout(() => {
+			history.push('/groups');
+		}, 5000);
+	};
+
+	return (
+		<main>
+			<h1 className='text-center text-6xl'>Signup</h1>
+
+			<form onSubmit={onSubmit}>
+				<div>
+					<label htmlFor='name'>Name</label>
+					<input name='name' id='name-input' type='text' className='bg-gray-300' value={formData.name} onChange={onChange} />
+				</div>
+
+				<button type='submit' className='bg-gray-400'>
+					Create Now!
+				</button>
+			</form>
+		</main>
+	);
+}
